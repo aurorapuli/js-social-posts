f69da2f9c997faff3eeee6d478bba6b9b6e048bc
aurorapuli <144793745+aurorapuli@users.noreply.github.com>
2023-12-05 15:36:13 +0100

    config: foreach

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+// Descrizione**
+// Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
+// Milestone 1 - Prendendo come riferimento il layout di esempio presente nell’html, stampiamo i post del nostro feed, prendendo le informazioni che ci servono dall’array di oggetti che già trovate.
+// Milestone 2 - Se clicchiamo sul tasto “Mi Piace” cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo. Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
+
+
+
+
 const posts = [
     {
         "id": 1,
@@ -55,3 +63,55 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+// riporto l'elemento in cui andrò a inserire l'HTML
+
+const post = document.getElementsByClassName('post');
+
+console.log(post);
+
+posts.forEach((element) => {
+
+
+    // creo l'Html da inserire in pagina
+    
+    const postUtente =
+    `
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${element.author.image}" alt="${element.author.name}">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">Phil Mangione</div>
+                    <div class="post-meta__time">${element.created}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${element.content}</div>
+        <div class="post__image">
+            <img src="${element.media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="${element.id}">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
+                </div>
+            </div> 
+        </div>
+    `
+
+    post.innerHTML += postUtente;
+
+
+});
+
+
+
